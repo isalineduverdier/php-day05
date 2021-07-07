73cfbff2180f395b28bd49c9fe1822f720a6f766
isaline <isaline.rochon-du-verdier@epitech.eu>
2021-07-07 14:56:29 +0200

    index.php

--- ex_05/index.php
+++ ex_05/index.php
@@ -1,1 +1,9 @@
 <?php
+if (isset ($_GET["name"]) !=NULL){
+    setcookie("name",$_GET["name"],time() +36000);
+}
+if (isset($_COOKIE["name"]) !=NULL){
+        echo "Hello " . $_COOKIE["name"];
+} else
+echo "Hello platypus";
+?>
